7c00e8eccbcd720fd4c5a200a5bbc5ed428b29ec
Grillai <53846286+Grillai@users.noreply.github.com>
2024-04-13 19:47:11 +0200

    sito

--- app.js
+++ app.js
@@ -112,6 +112,18 @@ gsap.to(".Parte_Destra-4",{
     }
 });
 
+gsap.to(".Contenitore_Prodotti", {
+    x: 0, // sposta l'elemento a x = 0, è partito da x = 100% // Imposta l'opacità a 1 per far apparire il contenitore
+    duration: 1, // durata dell'animazione in secondi
+    scrollTrigger: {
+      trigger: ".Prodotto", // l'animazione si attiva quando questo elemento è visibile a schermo
+      start: "top 70%", // l'animazione inizia quando il 30% superiore dell'elemento è visibile
+      end: "bottom 70%", // l'animazione termina quando il 30% inferiore dell'elemento è visibile
+      scrub: true // l'animazione si aggiorna mentre si scorre
+    }
+});
+  
+
 
 
 
@@ -122,4 +134,14 @@ document.addEventListener('DOMContentLoaded', function () {
     hamburgerMenu.addEventListener('click', function () {
         menuItems.classList.toggle('active');
     });
+});
+
+
+document.getElementById('toggleModulo').addEventListener('click', function() {
+    var modulo = document.getElementById('moduloContatto');
+    if (modulo.style.opacity === '0' || modulo.style.opacity === '') {
+        modulo.style.opacity = '1';
+    } else {
+        modulo.style.opacity = '0';
+    }
 });
